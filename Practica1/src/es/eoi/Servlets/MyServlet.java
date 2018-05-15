fb30b4c81382a1557ab5c3cf1d559fde0a802e56
Labte <Labte@10.100.5.50>
2018-05-15 16:09:46 +0200

    Pantalla todo de verde. OK COMMIT

---
+++ Practica1/src/es/eoi/Servlets/MyServlet.java
@@ -0,0 +1,63 @@
+package es.eoi.Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//Estas dos lineas se quitan, y todo lo referido al html.
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String nombre = request.getParameter("nombre");
+		String password = request.getParameter("password");
+		try {
+			
+			out.println("Nombre:\t"+nombre);
+			out.println("<br>");
+			out.println("Contraseña:\t"+password);
+			 
+		} finally {
+			
+			if( out != null ) {
+			
+				out.close();
+				
+			}
+			
+			
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
